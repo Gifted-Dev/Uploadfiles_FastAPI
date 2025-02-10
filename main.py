786383063cc129a64d8e4b5e63a6103e6897af88
Gifted <epopoola15@gmail.com>
2025-02-10 05:42:31 +0100

    FastAPI code to uploadfiles

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 import os # for handling file directories
 import shutil # for copying file data
+import uuid
 
 UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 # create a fastapi instance
 app = FastAPI()
@@ -11,8 +13,11 @@ app = FastAPI()
 @app.post("/small_files_upload")
 async def small_files(file: UploadFile = File(...)):
     
+    # generate a unique file id
+    file_id = str(uuid.uuid4())
+    
     # path to save the uploaded file
-    file_location = os.path.join(UPLOAD_DIR, file.filename)
+    file_location = os.path.join(UPLOAD_DIR, f"{file_id}_{file.filename}")
     
     # Open teh file in write-binary mode and save it
     with open(file_location, "wb") as buffer:
@@ -22,20 +27,22 @@ async def small_files(file: UploadFile = File(...)):
 
 # /*********** CODE TO UPLOAD LARGE FILES IN CHUNKS***************************/
 
-@app.post("large_files")
+@app.post("/large_files")
 async def large_files(file: UploadFile = File(...)):
     file_size = 0
     
     max_size = 1024 * 1024 * 1024 # setting max size to 1gb
     
-    # path to ave the uploaded file
-    file_location = os.path.join(UPLOAD_DIR, file.filename)
+    # generate a unique file id
+    file_id = str(uuid.uuid4())
+    
+    # path to save the uploaded file
+    file_location = os.path.join(UPLOAD_DIR, f"{file_id}_{file.filename}")
 
     with open(file_location, "wb") as buffer:
         while chunk := file.file.read(1024 * 1024):
             file_size += len(chunk)
             if file_size > max_size:
-               buffer.close()
                os.remove(file_location)
                raise HTTPException(status_code = 413, detail = "File too large (max 1GB)")
             buffer.write(chunk)
